8b62cae13266ce9458fdcdb7293f06a71d2a8a82
muirisc <91299876+muirisc@users.noreply.github.com>
2021-11-01 17:57:15 -0400

    commit tree c75a6388ad30137bdfbb27462583b5ae99304f2e

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks - 42)
+}
+
+function distanceFromHqInFeet(blocks){
+
+return distanceFromHqInBlocks(blocks) * 264
+}
+
+function distanceTravelledInFeet(startingblock,endingblock){
+
+    return Math.abs(startingblock - endingblock) * 264 
+
+}
+
+function calculatesFarePrice(startingblock,endingblock){
+
+if (distanceTravelledInFeet(startingblock,endingblock) > 2500){
+    return "cannot travel that far"
+}
+else if (distanceTravelledInFeet(startingblock,endingblock) > 2000) {
+    return 25
+}
+else if (distanceTravelledInFeet(startingblock,endingblock) > 400 && distanceTravelledInFeet(startingblock,endingblock) <= 2000){
+    return (distanceTravelledInFeet(startingblock,endingblock) - 400) * .02
+}
+else {
+    return 0
+}
+}
